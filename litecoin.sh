c304645e9f9e9c72510bbe41f997470ae68c5acd
SHouvik Ghosh <shouvikghosh87@gmail.com>
2018-09-22 12:07:24 +0530

    2nd commit

--- litecoin.sh
+++ litecoin.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+who=`whoami`
+if [ $who != "root" ];then
+echo "must run under root"
+exit
+else
 echo "downloding Litecoin"
 curl -o /tmp/litecoin.tar.gz https://download.litecoin.org/litecoin-0.16.3/linux/litecoin-0.16.3-x86_64-linux-gnu.tar.gz
 cd /tmp
 tar -xzf litecoin.tar.gz
 cp litecoin-0.16.3/bin/litecoind /usr/bin/
 cp litecoin-0.16.3/bin/litecoin-cli /usr/bin/
+sudo addgroup --system litecoin
+sudo adduser --system --home /var/lib/litecoin --ingroup litecoin litecoin
+mkdir /etc/litecoin
+touch /etc/litecoin/litecoin.conf
+
+fi
 
